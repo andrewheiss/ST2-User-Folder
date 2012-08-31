40e3964a679fc467c6f35e2aa2008d5ff6cafe3d
Andrew Heiss <andrewheiss@gmail.com>
2012-08-30 21:30:47 -0400

    Updated Skim page number plugin and added bash build system
    Among other minor changes

--- Clean page numbers.py
+++ Clean page numbers.py
@@ -5,15 +5,27 @@ class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.insert(edit, 0, "Hello, World!")
 
+class PrepareNumbersCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            if not region.empty():
+                text = self.view.substr(region)
+                text = re.sub("\\* Text Note, page (\\d{1,5})\\n", "\\1 - ", text)
+                self.view.replace(edit, region, text)
+
 class CleanNumbersCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # Find any string that starts with "## - "
-        digits = re.compile("^\\d* - ")
+        digits = re.compile("^\\d* - ")  # Find any string that starts with "## - "
         old_page = ''
         cleaned = ''
         for region in self.view.sel():
             if not region.empty():
-                for line in self.view.substr(region).splitlines():
+                # Find/replace all the Skim junk (* Text Note, page 5) with cleaner text (5 - )
+                text = self.view.substr(region)
+                text = re.sub("\\* Text Note, page (\\d{1,5})\\n", "\\1 - ", text)
+
+                # for line in self.view.substr(region).splitlines():
+                for line in text.splitlines():
                     check_for_digits = digits.match(line)
 
                     if check_for_digits:
